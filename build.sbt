25fd31b0575544eb9cc83b1b1d388c83f1da8d91
Lookuut Struchkov <lookuut@gmail.com>
2018-02-23 02:22:52 +0300

    Best result on CV Data 80 percent

--- build.sbt
+++ build.sbt
@@ -9,8 +9,7 @@ libraryDependencies += "org.specs2" %% "specs2-core" % "4.0.2" % "test"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1"
 libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.18.0"
 libraryDependencies += "joda-time" % "joda-time" % "2.9.9"
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.2.1"
 libraryDependencies += "com.meetup" %% "archery" % "0.4.0"
-
 
 scalacOptions in Test ++= Seq("-Yrangepos")
--- src/main/scala/com/lookuut/BankTransactions.scala
+++ src/main/scala/com/lookuut/BankTransactions.scala
@@ -24,6 +24,13 @@ import com.esri.dbscan.DBSCAN2
 import com.esri.dbscan.DBSCANPoint
 import java.io._
 
+import org.apache.spark.mllib.evaluation._
+import org.apache.spark.mllib.tree._
+import org.apache.spark.mllib.tree.model._
+import org.apache.spark.rdd._
+import org.apache.spark.mllib.linalg._
+import org.apache.spark.mllib.regression._
+
 
 class NAStatCounter extends Serializable {
 	val stats: StatCounter = new StatCounter()
@@ -64,6 +71,7 @@ object BankTransactions {
 	private val testDataFile = "/home/lookuut/Projects/raif-competition/resource/test_set.csv"
 	private val trainDataFile = "/home/lookuut/Projects/raif-competition/resource/train_set.csv"
 	private val smallTestDataFile = "/home/lookuut/Projects/raif-competition/resource/small_test_set.csv"
+	private val smallTrainDataFile = "/home/lookuut/Projects/raif-competition/resource/small_train_set.csv"
 
 	def run () {
 		val conf = new SparkConf().
@@ -162,9 +170,270 @@ object BankTransactions {
 		
 		val minDist = if (args.size >= 1) args(0).toDouble else 0.0035
 		val minPoint = if (args.size >= 2) args(1).toInt else 2
-		dbscan(minDist, minPoint)
+		
+		trainClassifier2
+		//dbscan(minDist, minPoint)
+		//new EchoBot("536920770:AAFL4Sbpb66tsZep0v2r4hTALyoUy9Q_yIc").run()
 	}
 
+	def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint]) : MulticlassMetrics = {
+		val predictionsAndLabels = data.map(example =>
+			(model.predict(example.features), example.label)
+		)
+		new MulticlassMetrics(predictionsAndLabels)
+	}
+
+	def trainClassifier2 () {
+		val conf = new SparkConf().
+						setAppName(applicationName).
+						setMaster("local[*]")		
+
+		val sparkContext = new SparkContext(conf)
+		val sqlContext = new SQLContext(sparkContext)
+		val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+
+		import sqlContext.implicits._
+		// Load and parse the data file.
+		val transactionDataRaw = sparkContext.textFile(trainDataFile)
+		
+		val trainTransactions = transactionDataRaw.filter(!_.contains("amount,atm_address,")).
+			zipWithIndex.
+			map{
+			case (line, index) => 
+				Transaction.parseTrainTransaction(line, index)
+			}.
+			filter(t => t.transaction.transactionPoint.getX > 0 && t.transaction.city.get == "NOVOSIBIRSK").
+			map(t => (t.homePoint.getX.toString + t.homePoint.getY.toString, t)).
+			cache()
+
+		val points = trainTransactions.
+			groupByKey.
+			mapValues{case(values) => values.head}.
+			zipWithIndex.
+			map{case((key, t), index) => 
+				DBSCANPoint(index, t.homePoint.getX, t.homePoint.getY)
+			}.collect()
+
+		val clusteredPoints = DBSCAN2(0.01, 2).
+								cluster(points).
+								map(p => (math.abs(p.clusterID), new Point(p.x, p.y)))
+		val maxClusterID = clusteredPoints.maxBy(_._1)._1 + 1
+
+		val zeroClusterPoints = clusteredPoints.
+			filter(_._1 == 0).
+			zipWithIndex.
+			map{
+				case((clusterId, point), index) =>
+					(index + maxClusterID, point)
+			}
+
+		val clusteredHomePoints = (zeroClusterPoints ++ clusteredPoints.filter(_._1 > 0)).
+			map(t => (t._2.getX.toString + t._2.getY.toString, t._1)).
+			toMap
+
+		println("Clusters count =====>" + clusteredHomePoints.valuesIterator.toStream.distinct.size)
+
+		val data = trainTransactions.
+			groupByKey.
+			mapValues{
+				case groupedTrans =>
+					var data = ListBuffer[Seq[Double]]()
+					var buffer = ListBuffer[Double]()
+					var index = 0
+					val customerTransactions = groupedTrans.
+						map{case (t) =>
+							val tPoint = t.transaction.transactionPoint
+							val hPoint = t.homePoint
+							val x = (tPoint.getX - hPoint.getX) 
+							val y = (tPoint.getY - hPoint.getY)
+							val squaredDistance = x * x + y * y
+							(squaredDistance, t)
+						}
+
+					val nearPurposePointTransactions = customerTransactions.
+						filter(t => t._1 <= squareScoreRadious)
+					
+					if (nearPurposePointTransactions.size > 0) {
+						scala.util.Random.shuffle(nearPurposePointTransactions).
+							foreach {
+								case (d, t) =>
+									buffer += t.transaction.transactionPoint.getX
+									buffer += t.transaction.transactionPoint.getY
+									buffer += t.transaction.amount.get
+									buffer += (if (t.transaction.atmPoint.getX > 0) 1 else 0).toDouble
+									index += 1
+
+									if (index >= 5) {
+										data += buffer.toSeq
+										buffer = ListBuffer[Double]()
+										index = 0
+									}
+							}
+					} else {
+						val t = customerTransactions.toSeq.sortWith(_._1 > _._1).head._2
+						buffer += t.transaction.transactionPoint.getX
+						buffer += t.transaction.transactionPoint.getY
+						buffer += t.transaction.amount.get
+						buffer += (if (t.transaction.atmPoint.getX > 0) 1 else 0).toDouble
+					}
+
+					if (buffer.size > 0) {
+						val lastPos = buffer.size
+						for (i <- buffer.size / 4 to 5 if i < 5) {
+							buffer += buffer(lastPos - 4)
+							buffer += buffer(lastPos - 3)
+							buffer += buffer(lastPos - 2)
+							buffer += buffer(lastPos - 1)
+						}	
+						data += buffer.toSeq
+					}
+					
+					data.toSeq
+			}.
+			flatMap{case (sPoint, values) => values.map((sPoint, _))}.
+			map{
+				case (sPoint, params) => 
+					LabeledPoint(
+						clusteredHomePoints.get(sPoint).get.toDouble,
+						Vectors.dense(
+							params.toArray
+						)
+					)
+			}
+
+		val Array(_trainData, _cvData) = data.randomSplit(Array(0.8, 0.2))
+		val trainData = _trainData.cache() 
+		val cvData = _cvData.cache()
+		// Train a DecisionTree model.
+		//  Empty categoricalFeaturesInfo indicates all features are continuous.
+		val categoricalFeaturesInfo = Map[Int, Int]()
+
+		val evaluations = for (
+								impurity <- Array("gini", "entropy");
+								depth <- Array(10, 20, 30);
+								bins <- Array(40, 100, 300)
+							) yield {
+								println(f"=============> Smart of $impurity, $depth, $bins")
+								val model = DecisionTree.trainClassifier(
+								trainData, clusteredHomePoints.valuesIterator.max.toInt + 1, categoricalFeaturesInfo,
+								impurity, depth, bins)
+								println(f"=============> End of $impurity, $depth, $bins")
+
+								val trainAccuracy = getMetrics(model, trainData).precision
+								val cvAccuracy = getMetrics(model, cvData).precision
+								((impurity, depth, bins), (trainAccuracy, cvAccuracy))
+							}
+
+		evaluations.sortBy(_._2).reverse.foreach(println)
+		
+	} 
+
+	def trainClassifier () {
+		val conf = new SparkConf().
+						setAppName(applicationName).
+						setMaster("local[*]")		
+
+		val sparkContext = new SparkContext(conf)
+		val sqlContext = new SQLContext(sparkContext)
+		val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+
+		import sqlContext.implicits._
+		// Load and parse the data file.
+		val transactionDataRaw = sparkContext.textFile(trainDataFile)
+		
+		val trainTransactions = transactionDataRaw.filter(!_.contains("amount,atm_address,")).
+			zipWithIndex.
+			map{
+			case (line, index) => 
+				Transaction.parseTrainTransaction(line, index)
+			}.
+			filter(t => t.transaction.transactionPoint.getX > 0).
+			filter{case (t) => 
+							val tPoint = t.transaction.transactionPoint
+							val hPoint = t.homePoint
+							val x = (tPoint.getX - hPoint.getX) 
+							val y = (tPoint.getY - hPoint.getY)
+							val distance = x * x + y * y
+							distance * distance <= squareScoreRadious
+			}.cache()
+
+		val homePointMap = trainTransactions.
+			map(t => (t.homePoint.getX.toString + t.homePoint.getY.toString, 1)).
+			groupByKey.
+			zipWithUniqueId.
+			map{case((key, value), index) => (key, index)}.
+			collectAsMap()
+
+		val cityMap = trainTransactions.
+			map(t => (t.transaction.city, 1)).
+			groupByKey.
+			zipWithUniqueId.
+			map{case((key, value), index) => (key, index)}.
+			collectAsMap()
+
+		val mccMap = trainTransactions.
+			map(t => (t.transaction.mcc, 1)).
+			groupByKey.
+			zipWithUniqueId.
+			map{case((key, value), index) => (key, index)}.
+			collectAsMap()
+
+	
+		println("==========>")
+		println(homePointMap.size)
+		println(cityMap.size)
+		println(mccMap.size)
+		
+		println("==========>")
+
+		val data = trainTransactions.
+			map{
+				case (t) => 
+					LabeledPoint(
+						homePointMap.get(t.homePoint.getX.toString + t.homePoint.getY.toString).get.toDouble,
+						Vectors.dense(
+							Array(
+								//cityMap.get(t.transaction.city).get.toDouble,
+								//mccMap.get(t.transaction.mcc).get.toDouble,
+								t.transaction.transactionPoint.getX,
+								t.transaction.transactionPoint.getY,
+								t.transaction.amount.getOrElse(0.0)
+							)
+						)
+					)
+			}.cache()
+
+		val Array(_trainData, _cvData) = data.randomSplit(Array(0.7, 0.3))
+		val trainData = _trainData.cache() 
+		val cvData = _cvData.cache()
+		// Train a DecisionTree model.
+		//  Empty categoricalFeaturesInfo indicates all features are continuous.
+		val categoricalFeaturesInfo = Map/*(
+											0 -> cityMap.size, 
+											1 -> mccMap.size
+										)*/[Int, Int]()
+
+		val evaluations = for (
+								impurity <- Array("gini", "entropy");
+								depth <- Array(30);
+								bins <- Array(300)
+							) yield {
+								println(f"=============> Smart of $impurity, $depth, $bins")
+								val model = DecisionTree.trainClassifier(
+								trainData, homePointMap.valuesIterator.max.toInt + 1, categoricalFeaturesInfo,
+								impurity, depth, bins)
+								println(f"=============> End of $impurity, $depth, $bins")
+
+								val trainAccuracy = getMetrics(model, trainData).precision
+								val cvAccuracy = getMetrics(model, cvData).precision
+								((impurity, depth, bins), (trainAccuracy, cvAccuracy))
+							}
+
+		evaluations.sortBy(_._2).reverse.foreach(println)
+	}
+
+	private val scoreRadious = 0.02
+	private val squareScoreRadious = 0.0004
 
 	def trainDataAnalytics() {
 		val conf = new SparkConf().
@@ -179,22 +448,59 @@ object BankTransactions {
 
 		val transactionDataRaw = sparkContext.textFile(trainDataFile)
 		
-		println("================>")
-
 		val transactions = transactionDataRaw.filter(!_.contains("amount,atm_address,")).
 			zipWithIndex.
 			map{
 			case (line, index) => 
 				Transaction.parseTrainTransaction(line, index)
-			}.filter(x => x.transaction.city.getOrElse("") == "MOSCOW" && x.workPoint.getX > 0)
-			.map {
-				case(trainTransaction) => 
-					val transaction = trainTransaction.transaction
-					val (distance1, angle1) = cartesianToPolar(transaction.transactionPoint, moscowCartesianCenter)
-					val (distance2, angle2) = cartesianToPolar(trainTransaction.workPoint, moscowCartesianCenter)
-					val (distance3, angle3) = cartesianToPolar(trainTransaction.homePoint, moscowCartesianCenter)
-					(transaction.amount, distance1, angle1, (distance2 * 1000))
-			}.take(100).foreach(println)
+			}.
+			filter(t => t.transaction.transactionPoint.getX > 0).
+			cache()
+
+		println("===>Train data count " + transactions.count())
+		val customerCount = transactions.map(_.transaction.customer_id).distinct().count()
+		val customersWithWorkCount = transactions.filter(_.workPoint.getX > 0).map(_.transaction.customer_id).distinct().count()
+		println("===>Customer count " + customerCount)
+		println("===>Customers with work count " + customersWithWorkCount)
+
+		val customers = transactions.
+			map(t => (t.transaction.customer_id, t)).
+			groupByKey.
+			mapValues{
+				case (customerTrans) => 
+					val nearHomePointsCount = customerTrans.
+						filter{case (t) => 
+								val tPoint = t.transaction.transactionPoint
+								val hPoint = t.homePoint
+								val x = (tPoint.getX - hPoint.getX) 
+								val y = (tPoint.getY - hPoint.getY)
+								val distance = x * x + y * y
+								distance * distance <= squareScoreRadious
+							}.size
+
+					val nearWorkPointsCount = customerTrans.
+						filter{
+							case(t) => 
+								val tPoint = t.transaction.transactionPoint
+								val hPoint = t.workPoint
+								val x = (tPoint.getX - hPoint.getX) * (tPoint.getX - hPoint.getX)
+								val y = (tPoint.getY - hPoint.getY) * (tPoint.getY - hPoint.getY)
+								val distance = x + y
+								distance * distance <= squareScoreRadious
+							}.size
+					val pointsCount = customerTrans.size
+					(pointsCount, nearHomePointsCount.toDouble / pointsCount, nearWorkPointsCount.toDouble / pointsCount.toDouble, nearHomePointsCount, nearWorkPointsCount)
+			}.cache()
+
+		val home10 = customers.filter(_._2._4 >= 10).count().toDouble / customerCount 
+		val home8 = customers.filter(_._2._4 >= 8).count().toDouble / customerCount
+		val home5 = customers.filter(_._2._4 >= 5).count().toDouble / customerCount
+
+		val work10 = customers.filter(_._2._5 >= 10).count().toDouble / customersWithWorkCount
+		val work8 = customers.filter(_._2._5 >= 8).count().toDouble / customersWithWorkCount
+		val work5 = customers.filter(_._2._5 >= 5).count().toDouble / customersWithWorkCount
+
+		println(f"===> home10 $home10, home8 $home8, home5 $home5 and work10 $work10, work8 $work8, work5 $work5")
 	}
 
 	private val moscowCartesianCenter = new Point(55.752818, 37.621754)
